59fc21f509c5a14fdb5644c28e1d278e6dc9aef2
GreenSky <trachukyulia77@gmail.com>
2023-09-17 21:53:39 +0300

    added import and export data to (from) database

--- FolderStructure/Pages/Index.cshtml.cs
+++ FolderStructure/Pages/Index.cshtml.cs
@@ -7,25 +7,32 @@ namespace FolderStructure.Pages
     public class IndexModel : PageModel
     {
         private readonly FolderContext _context;
-        public Folder Folder { get; set; }
-        public List<Folder> Folders { get; set; }
-        public List<Folder> ChildFolders { get; set; }
+        public Folder? Folder { get; set; }
+        public List<Folder>? Folders { get; set; }
+        public List<Folder>? ChildFolders { get; set; }
+        public string? Message { get; set; }
         public IndexModel(FolderContext db)
         {
             _context = db;
         }
-
         public IActionResult OnGet(int? id)
         {
-            if (id == null)
+            try
             {
-                id = 1;
+                if (id == null)
+                {
+                    id = 1;
+                }
+                Folder = _context.Folder.FirstOrDefault(f => f.Id == id);
+                ChildFolders = _context.Folder
+                 .Where(f => f.ParentId == Folder.Id)
+                 .ToList();
+                Folders = _context.Folder.ToList();
             }
-            Folder = _context.Folder.FirstOrDefault(f => f.Id == id);
-            ChildFolders = _context.Folder
-             .Where(f => f.ParentId == Folder.Id)
-             .ToList();
-            Folders = _context.Folder.ToList();
+            catch
+            {
+                Message = "No folder found.";
+            }
             return Page();
         }
     }
--- FolderStructure/Pages/Upload.cshtml.cs
+++ FolderStructure/Pages/Upload.cshtml.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FolderStructure.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
 using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace FolderStructure.Pages
@@ -7,9 +10,12 @@ namespace FolderStructure.Pages
     public class UploadModel : PageModel
     {
         private IHostingEnvironment _environment;
-        public UploadModel(IHostingEnvironment environment)
+        private readonly FolderContext _context;
+
+        public UploadModel(IHostingEnvironment environment, FolderContext context)
         {
             _environment = environment;
+            _context = context;
         }
         [BindProperty]
         public string Message { get; set; }
@@ -23,9 +29,57 @@ namespace FolderStructure.Pages
                 {
                     await UploadFile.CopyToAsync(fileStream);
                 }
-                Message = "File uploaded successfully!";
+                ExportData();
+                ClearRows();
+                ImportDataFromTxtFile(file);
             }
-           // return RedirectToPage("/Index");
+            // return RedirectToPage("/Index");
+        }
+        public void ImportDataFromTxtFile(string filePath)
+        {
+            try
+            {
+                var lines = System.IO.File.ReadAllLines(filePath);
+                foreach (var line in lines)
+                {
+                    var data = line.Split(',');
+                        var newItem = new Folder
+                        {
+                            Id = int.Parse(data[0]),
+                            Name = data[1],
+                            ParentId = int.Parse(data[2])
+                        };
+                        _context.Folder.Add(newItem);
+                    _context.SaveChanges();
+                }
+                Message = "File uploaded successfully!";
+
+            }
+            catch
+            {
+                Message = "Oops, data in the file isn't correct";
+            }
+        }
+        public void ExportData()
+        {
+            var dataToExport = _context.Folder.ToList();
+
+            string exportFileName = "exported_data.txt";
+            string filePath = Path.Combine(_environment.ContentRootPath, "uploads", exportFileName);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var item in dataToExport)
+                {
+                    writer.WriteLine($"{item.Id},{item.Name},{item.ParentId}");
+                }
+            }
+        }
+        public void ClearRows()
+        {
+            var tableData = _context.Folder;
+            tableData.RemoveRange(tableData);
+            _context.SaveChanges();
         }
 
     }
