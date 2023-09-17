03c897f63fa050ff08d001d7b767927d229e45a4
GreenSky <trachukyulia77@gmail.com>
2023-09-17 20:05:10 +0300

    added uploading files

--- FolderStructure/Pages/Index.cshtml.cs
+++ FolderStructure/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using FolderStructure.Models;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace FolderStructure.Pages
@@ -6,14 +7,22 @@ namespace FolderStructure.Pages
     public class IndexModel : PageModel
     {
         private readonly FolderContext _context;
-
-        public IndexModel(FolderContext db)
-        {
-            _context = db;
-        }
+        private readonly IWebHostEnvironment _environment;
         public Folder Folder { get; set; }
         public List<Folder> Folders { get; set; }
         public List<Folder> ChildFolders { get; set; }
+        [BindProperty]
+        public IFormFile File { get; set; }
+        [BindProperty]
+        public IFormFile FormFile { get; set; }
+
+        public string Message { get; set; }
+        public IndexModel(FolderContext db, IWebHostEnvironment environment)
+        {
+            _context = db;
+            _environment = environment;
+
+        }
 
         public IActionResult OnGet(int? id)
         {
@@ -21,12 +30,12 @@ namespace FolderStructure.Pages
             {
                 id = 1;
             }
-                Folder = _context.Folder.FirstOrDefault(f => f.Id == id);
-                ChildFolders = _context.Folder
-                 .Where(f => f.ParentId == Folder.Id)
-                 .ToList();
-                Folders = _context.Folder.ToList();
+            Folder = _context.Folder.FirstOrDefault(f => f.Id == id);
+            ChildFolders = _context.Folder
+             .Where(f => f.ParentId == Folder.Id)
+             .ToList();
+            Folders = _context.Folder.ToList();
             return Page();
-        } 
+        }
     }
 }
---
+++ FolderStructure/Pages/Upload.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace FolderStructure.Pages
+{
+    public class UploadModel : PageModel
+    {
+        private IHostingEnvironment _environment;
+        public UploadModel(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        [BindProperty]
+        public string Message { get; set; }
+        public IFormFile UploadFile { get; set; }
+        public async Task OnPostAsync()
+        {
+            if (UploadFile != null && UploadFile.Length > 0)
+            {
+                var file = Path.Combine(_environment.ContentRootPath, "uploads", UploadFile.FileName);
+                using (var fileStream = new FileStream(file, FileMode.Create))
+                {
+                    await UploadFile.CopyToAsync(fileStream);
+                }
+                Message = "File uploaded successfully!";
+            }
+           // return RedirectToPage("/Index");
+        }
+
+    }
+}
